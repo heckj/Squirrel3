0a90500365e1c6959fd2e9614ab4687817627404
Joseph Heck <heckj@mac.com>
2022-01-01 17:04:29 -0800

    adding seedable protocol to Squirrel3, and making the seed constant for easier retrieval and re-creation (#1)

--- Sources/Squirrel3/Squirrel3.swift
+++ Sources/Squirrel3/Squirrel3.swift
@@ -1,22 +1,25 @@
-import Foundation
 import CSquirrel
+import Foundation
 // This RNG function uses Squirrel Eiserloh's hash function to generate "random" numbers.
 
-
 /// A Pseudo-random number generator that deterministically creates random numbers based on the seed you provide.
-public struct PRNG: RandomNumberGenerator {
+public struct PRNG: SeededRandomNumberGenerator {
+    /// The current position of the pseudo-random number generator.
+    public var position: UInt64
+
     /// The seed for the pseudo-random number generator.
-    public var seed: UInt64
-    
+    public let seed: UInt64
+
     /// Creates a new pseudo-random number generator.
     /// - Parameter seed: An unsigned integer seed value for the generator.
     public init(seed: UInt64) {
         self.seed = seed
+        position = seed
     }
-    
+
     /// Returns the next random UInt64 value.
     public mutating func next() -> UInt64 {
-        seed = Squirrel3(seed)
-        return seed
+        position = Squirrel3(position)
+        return position
     }
 }
